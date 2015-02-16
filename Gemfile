2503c9a2b2783f35ab4489a53a71281116bd52d5
ferateck <summer66@msu.edu>
2015-02-16 18:32:39 -0500

    done and it looks good

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.1.5'
 
 gem 'sinatra', '~> 1.4.5'
 gem 'holidapi'
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
-		erb :"index#{ rand(3) + 1 }"
+		date = Time.now
+		year = date.year
+		month = date.month
+		@holidays = HolidApi.get(country: 'US', year: year, month: month).flatten
+		@birthday = HolidApi.get(country: 'US', year: 1983, month: 9).flatten
+		erb :index1
 	end
 end
